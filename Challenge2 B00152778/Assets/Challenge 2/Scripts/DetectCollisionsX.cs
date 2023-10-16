90112a7c45852d3dbc96c99278d700b62c0097b3
Femi-coder <B00152778@mytudublin.ie>
2023-10-16 21:48:32 +0100

    Unit2

---
+++ Challenge2 B00152778/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Dog"))
+        {
+            // Destroy the GameObject only if it's not a "Dog"
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Challenge2 B00152778/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogSpawnRate = 1; 
+    private float nextDogSpawnTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextDogSpawnTime)
+        {
+            nextDogSpawnTime = Time.time + dogSpawnRate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
